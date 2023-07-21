a20c7800b6c1b88291ffcef2eacdd5b0fa74b32d
koushalkhajuria <koushalkhajuria9680@gmail.com>
2023-07-21 19:56:29 +0530

    updates

--- Back_end/app.js
+++ Back_end/app.js
@@ -14,13 +14,16 @@ app.use(bodyParser.json({ limit: '20mb' }));
 // app.use(express.json({ limit: '10mb' }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://64ba921442cc08067b4568f2--genuine-truffle-fa1a52.netlify.app/'
+  );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (e.g., cookies)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   next();
 });
