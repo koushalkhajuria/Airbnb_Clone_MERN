7267038f31de2312e327f164e3d6687d171d28f7
Koushal Khajuria <68377402+koushalkhajuria@users.noreply.github.com>
2023-07-21 21:18:55 +0530

    Update axios.js

--- Front_end/src/services/axios.js
+++ Front_end/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://mern-stack-clone.netlify.app/api/v1';
+const BASE_URL = 'https://mern-stack-clone-airbnb.onrender.com/api/v1';
 
 export const axiosPrivate = axios.create(
   {baseURL: BASE_URL,
