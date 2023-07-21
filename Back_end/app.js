dbd46b396f6457242652776aced0ca6938c5e3da
Koushal Khajuria <68377402+koushalkhajuria@users.noreply.github.com>
2023-07-21 21:16:35 +0530

    Update app.js

--- Back_end/app.js
+++ Back_end/app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json({ limit: '20mb' }));
 // });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mern-stack-clone.netlify.app');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
